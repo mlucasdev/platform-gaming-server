29cab5c9f8840093507662a7b020769fb9e8a3d4
mlucasdev <lucasmsantos819@gmail.com>
2022-05-23 15:07:36 -0300

    feat(game): implement `class-validator`

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,9 +1,45 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateGameDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   coverImageUrl: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(100)
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   year: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   imdbScore: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
   trailerYoutubeUrl: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
   gameplayYoutubeUrl: string;
 }
