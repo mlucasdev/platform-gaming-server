20847118b91698f58e1a2e33e846de1a53b52f92
mlucasdev <lucasmsantos819@gmail.com>
2022-05-23 11:12:47 -0300

    feat(game): implement create game endpoint

---
+++ src/game/entities/game.entity.ts
@@ -0,0 +1,10 @@
+export class Game {
+  id: number;
+  title: string;
+  coverImageUrl: string;
+  description: string;
+  year: number;
+  imdbScore: number;
+  trailerYoutubeUrl: string;
+  gameplayYoutubeUrl: string;
+}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
+import { Game } from './entities/game.entity';
 
 @Injectable()
 export class GameService {
+  games: Game[] = [];
   create(createGameDto: CreateGameDto) {
-    return 'Criar um novo jogo.';
+    const game: Game = { id: 1, ...createGameDto };
+    this.games.push(game);
+    return game;
   }
 
   findAll() {
-    return 'Buscar todos os jogos.';
+    return 'Buscar todos os jogosa.';
   }
 }
