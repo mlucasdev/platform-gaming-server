21d64e2f07f54f202014bcab2ec41b1e675d7bab
mlucasdev <lucasmsantos819@gmail.com>
2022-05-23 15:14:54 -0300

    feat(game): implement `findOne`

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { GameService } from './game.service';
 
@@ -8,11 +8,17 @@ export class GameController {
 
   @Post()
   create(@Body() createGameDto: CreateGameDto) {
-    return this.gameService.create(createGameDto);
+    const randomId: number = Math.floor(Math.random() * (999999 - 1)) + 1;
+    return this.gameService.create(randomId, createGameDto);
   }
 
   @Get()
   findAll() {
     return this.gameService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.gameService.findOne(id);
   }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -5,13 +5,17 @@ import { Game } from './entities/game.entity';
 @Injectable()
 export class GameService {
   games: Game[] = [];
-  create(createGameDto: CreateGameDto) {
-    const game: Game = { id: 1, ...createGameDto };
+  create(randomId: number, createGameDto: CreateGameDto) {
+    const game: Game = { id: randomId, ...createGameDto };
     this.games.push(game);
     return game;
   }
 
   findAll() {
     return this.games;
+  }
+
+  findOne(id: number) {
+    return this.games.find(element => element.id == id)
   }
 }
