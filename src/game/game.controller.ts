f2dd6b946e51fb9ac33b480ee7f18a16ff11060e
mlucasdev <lucasmsantos819@gmail.com>
2022-05-23 21:06:44 -0300

    feat(game): implement `ApiOperation`

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -7,7 +7,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { GameService } from './game.service';
@@ -18,22 +18,34 @@ export class GameController {
   constructor(private gameService: GameService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar um novo jogo.',
+  })
   create(@Body() createGameDto: CreateGameDto): CreateGameDto {
     const randomId: number = Math.floor(Math.random() * (999999 - 1)) + 1;
     return this.gameService.create(randomId, createGameDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Buscar todos os jogos.',
+  })
   findAll(): CreateGameDto[] {
     return this.gameService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Buscar um jogo pelo id.',
+  })
   findOne(@Param('id') id: number): CreateGameDto {
     return this.gameService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary: 'Editar um jogo pelo id.',
+  })
   update(
     @Param('id') id: number,
     @Body() updateGameDto: UpdateGameDto,
@@ -42,6 +54,9 @@ export class GameController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar um jogo pelo id.',
+  })
   delete(@Param('id') id: number): object {
     return this.gameService.delete(id);
   }
