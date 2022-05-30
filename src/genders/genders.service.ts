bde96754bac88010ce0f0c29067190c12e5f9076
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 16:10:22 -0300

    feat(gender): implement not found error in `findAll`

--- src/genders/genders.service.ts
+++ src/genders/genders.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
@@ -10,11 +15,15 @@ export class GendersService {
 
   create(dto: CreateGenderDto): Promise<Gender> {
     const data: Gender = { ...dto };
-    return this.prisma.genders.create({ data });
+    return this.prisma.genders.create({ data }).catch(this.handleError);
   }
 
-  findAll(): Promise<Gender[]> {
-    return this.prisma.genders.findMany();
+  async findAll(): Promise<Gender[]> {
+    const genders = await this.prisma.genders.findMany();
+    if (genders.length == 0) {
+      throw new NotFoundException(`Nada foi encontrado.`);
+    }
+    return genders;
   }
 
   findOne(id: string): Promise<Gender> {
@@ -24,7 +33,9 @@ export class GendersService {
   async update(id: string, dto: UpdateGenderDto): Promise<Gender> {
     await this.findById(id);
     const data: Partial<Gender> = { ...dto };
-    return this.prisma.genders.update({ where: { id }, data });
+    return this.prisma.genders
+      .update({ where: { id }, data })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
@@ -39,5 +50,14 @@ export class GendersService {
       throw new NotFoundException(`Registro com o Id '${id}' não encontrado.`);
     }
     return record;
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
+    );
   }
 }
