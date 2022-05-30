d4717a7c152f8d7f0fcafb675dd1a59384f23e3b
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 13:55:54 -0300

    feat(gender): create dto and entity

--- src/genders/dto/create-gender.dto.ts
+++ src/genders/dto/create-gender.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGenderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateGenderDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do Gênero.',
+    example: 'FPS',
+  })
+  name: string;
+}
