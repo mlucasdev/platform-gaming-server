362eb829f98b247e9ab254317063fefc0f8c3671
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 14:09:37 -0300

    feat(gender): addTag `gender`

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .addTag('status')
     .addTag('game')
+    .addTag('gender')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
