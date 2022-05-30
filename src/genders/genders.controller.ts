c1dce19231689806ad062613808bc7aa7a195c4d
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 14:10:12 -0300

    feat(gender): implement `create` and `findAll`

--- src/genders/genders.controller.ts
+++ src/genders/genders.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { GendersService } from './genders.service';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Gender } from './entities/gender.entity';
 
+@ApiTags('gender')
 @Controller('genders')
 export class GendersController {
   constructor(private readonly gendersService: GendersService) {}
 
   @Post()
-  create(@Body() createGenderDto: CreateGenderDto) {
-    return this.gendersService.create(createGenderDto);
+  create(@Body() dto: CreateGenderDto): Promise<Gender> {
+    return this.gendersService.create(dto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Gender[]> {
     return this.gendersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.gendersService.findOne(+id);
+    return this.gendersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGenderDto: UpdateGenderDto) {
-    return this.gendersService.update(+id, updateGenderDto);
+  update(@Param('id') id: string, @Body() dto: UpdateGenderDto) {
+    return this.gendersService.update(id, dto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.gendersService.remove(+id);
+    return this.gendersService.remove(id);
   }
 }
--- src/genders/genders.service.ts
+++ src/genders/genders.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
+import { Gender } from './entities/gender.entity';
 
 @Injectable()
 export class GendersService {
-  create(createGenderDto: CreateGenderDto) {
-    return 'This action adds a new gender';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateGenderDto): Promise<Gender> {
+    const data: Gender = { ...dto };
+    return this.prisma.genders.create({ data });
   }
 
-  findAll() {
-    return `This action returns all genders`;
+  findAll(): Promise<Gender[]> {
+    return this.prisma.genders.findMany();
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} gender`;
   }
 
-  update(id: number, updateGenderDto: UpdateGenderDto) {
+  update(id: string, dto: UpdateGenderDto) {
     return `This action updates a #${id} gender`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} gender`;
   }
 }
