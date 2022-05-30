1bb7a16b098558aa1e6b049dcfdffc517f3e2dd5
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 16:02:52 -0300

    feat(table): correction `handleError`

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -32,10 +32,12 @@ export class GameService {
     await this.findById(id);
     this.imdbScoreVerify(dto);
     const data: Partial<Game> = { ...dto };
-    return this.prisma.games.update({
-      where: { id },
-      data,
-    });
+    return this.prisma.games
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
@@ -57,7 +59,7 @@ export class GameService {
     const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
 
     throw new UnprocessableEntityException(
-      errorLines[lastErrorLine] || 'Algum erro ocorreu ao executar a operação.',
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
     );
   }
 
