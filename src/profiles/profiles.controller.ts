6f25fecc6fa44222637d259605898067c8f63d54
mlucasdev <lucasmsantos819@gmail.com>
2022-05-30 21:47:25 -0300

    feat(profile): implement crud

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -2,33 +2,54 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProfilesService } from './profiles.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Profile } from './entities/profile.entity';
 
+@ApiTags('profile')
 @Controller('profiles')
 export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
   @Post()
-  create(@Body() createProfileDto: CreateProfileDto) {
-    return this.profilesService.create(createProfileDto);
+  @ApiOperation({
+    summary: 'Criar um novo jogo.',
+  })
+  create(@Body() dto: CreateProfileDto): Promise<Profile> {
+    return this.profilesService.create(dto);
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'Buscar todos os jogos.',
+  })
+  findAll(): Promise<Profile[]> {
     return this.profilesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.profilesService.findOne(+id);
+  @ApiOperation({
+    summary: 'Buscar um jogo pelo ID.',
+  })
+  findOne(@Param('id') id: string): Promise<Profile> {
+    return this.profilesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
-    return this.profilesService.update(+id, updateProfileDto);
+  @ApiOperation({
+    summary: 'Editar um jogo pelo ID.',
+  })
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateProfileDto,
+  ): Promise<Profile> {
+    return this.profilesService.update(id, dto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.profilesService.remove(+id);
-  }
+  // @Delete(':id')
+  // @ApiOperation({
+  //   summary: 'Deletar um jogo pelo ID.',
+  // })
+  // delete(@Param('id') id: string): Promise<void> {
+  //   return this.profilesService.delete(id);
+  // }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Profile } from './entities/profile.entity';
 
 @Injectable()
 export class ProfilesService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProfileDto): Promise<Profile> {
+    const data: Profile = { ...dto };
+    return this.prisma.profiles.create({ data }).catch(this.handleError);
   }
 
-  findAll() {
-    return `This action returns all profiles`;
+  async findAll(): Promise<Profile[]> {
+    const profiles = await this.prisma.profiles.findMany();
+    if (profiles.length == 0) {
+      throw new NotFoundException(`Nada foi encontrado.`);
+    }
+    return profiles;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  findOne(id: string): Promise<Profile> {
+    return this.findById(id);
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+  async update(id: string, dto: UpdateProfileDto): Promise<Profile> {
+    await this.findById(id);
+    const data: Partial<Profile> = { ...dto };
+    return this.prisma.profiles
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+  // async delete(id: string) {
+  //   await this.findById(id);
+  //   await this.prisma.profiles.delete({ where: { id } });
+  //   throw new HttpException('', 204);
+  // }
+
+  async findById(id: string): Promise<Profile> {
+    const record = await this.prisma.profiles.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado.`);
+    }
+    return record;
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
+    );
   }
 }
