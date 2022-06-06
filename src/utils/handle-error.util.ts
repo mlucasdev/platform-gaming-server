3544d604f5f5f6fc4cf40e8b5fd1d4cfaf11dda0
mlucasdev <lucasmsantos819@gmail.com>
2022-06-06 10:27:34 -0300

    feat: implement `console.error`

--- src/utils/handle-error.util.ts
+++ src/utils/handle-error.util.ts
@@ -4,6 +4,10 @@ export const handleError = (error: Error): undefined => {
   const errorLines = error.message?.split('\n');
   const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
 
+  if (!lastErrorLine) {
+    console.error(error);
+  }
+
   throw new UnprocessableEntityException(
     lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
   );
