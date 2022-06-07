dcdf7d090071e812b8e8bf8d89de7f97029c746f
mlucasdev <lucasmsantos819@gmail.com>
2022-06-07 16:06:52 -0300

    feat(profileGame): implement `create`

--- src/profile-game/profile-game.controller.ts
+++ src/profile-game/profile-game.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateProfileGameDto } from './dto/create-profile-game.dto';
+import { ProfileGameService } from './profile-game.service';
 
 @Controller('profile-game')
-export class ProfileGameController {}
+export class ProfileGameController {
+  constructor(private readonly profileGameService: ProfileGameService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Adicionar um jogo a um perfil e definir se é um jogo favorito',
+  })
+  create(@Body() dto: CreateProfileGameDto) {
+    return this.profileGameService.create(dto);
+  }
+}
--- src/profile-game/profile-game.service.ts
+++ src/profile-game/profile-game.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
+import { CreateProfileGameDto } from './dto/create-profile-game.dto';
 
 @Injectable()
-export class ProfileGameService {}
+export class ProfileGameService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProfileGameDto) {
+    const data: Prisma.ProfileGameCreateInput = {
+      game: {
+        connect: {
+          id: dto.gameId,
+        },
+      },
+      profile: {
+        connect: {
+          id: dto.profileId,
+        },
+      },
+      favorite: dto.favorite,
+    };
+
+    return this.prisma.profileGame
+      .create({
+        data,
+        select: {
+          game: {
+            select: {
+              id: true,
+              title: true,
+              coverImageUrl: true,
+              genres: {
+                select: {
+                  genre: {
+                    select: {
+                      name: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          profile: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+          favorite: true,
+        },
+      })
+      .catch(handleError);
+  }
+}
