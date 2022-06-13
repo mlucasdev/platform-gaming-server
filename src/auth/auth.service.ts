6b4e6d8d67e225b1b3aaf666fedcfa7571636dad
mlucasdev <lucasmsantos819@gmail.com>
2022-06-13 11:33:54 -0300

    feat(auth): implement jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginRespondeDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(dto: LoginDto): Promise<LoginRespondeDto> {
     const { email, password } = dto;
@@ -26,7 +30,7 @@ export class AuthService {
     delete user.password;
 
     return {
-      token: 'Token teste',
+      token: this.jwtService.sign({ email }),
       user: user,
     };
   }
